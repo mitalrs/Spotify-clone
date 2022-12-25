1e82ebc8f4da61d391f46f5bb25c5430f03fc1ae
mitalrs <mitalsapkale1999@gmail.com>
2022-12-25 12:51:40 +0530

    working on after log in

--- spotify/src/login/login.js
+++ spotify/src/login/login.js
@@ -1,6 +1,8 @@
 const CLIENT_ID = "19f033cc2db7431f9de61a310e7704bf";
 const scopes = "user-top-read user-follow-read playlist-read-private user-library-read";
 const REDIRECT_URI = "http://localhost:3000/login/login.html"
+const ACCESS_TOKEN_KEY = "accessToken";
+const APP_URL = "http://localhost:3000";
 
 const authorizeUser = ()=>{
     const url = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${REDIRECT_URI}&scope=${scopes}&show_dialog=true`;
@@ -10,4 +12,11 @@ const authorizeUser = ()=>{
 document.addEventListener("DOMContentLoaded", ()=>{
     const loginButton = document.getElementById("login-spotify");
     loginButton.addEventListener("click", authorizeUser);
+})
+
+window.addEventListener("load", ()=>{
+    const accessToken = localStrorage.getItem(ACCESS_TOKEN_KEY);
+    if(accessToken){
+        window.location.href = "http://location"
+    }
 })
