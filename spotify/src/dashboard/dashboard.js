84be55cda6234b5a2c51af0eef5b36cffeee9511
mitalrs <mitalsapkale1999@gmail.com>
2022-12-31 20:19:17 +0530

    daily commit

--- spotify/src/dashboard/dashboard.js
+++ spotify/src/dashboard/dashboard.js
@@ -2,13 +2,21 @@ import { fetchRequest } from "../api";
 import { ENDPOINT } from "../comman";
 
 const loadUserProfile = async () => {
-    const dafaultImg = document.querySelector("#default-img");
+    const defaultImg = document.querySelector("#default-img");
     const profileButton = document.querySelector("#user-profile-btn");
     const diplayNameElement = document.querySelector("#uer-name");
 
 
     const {diplay_name:diplayName, images } = await fetchRequest(ENDPOINT.userInfo);
+
+
+    if(images?.length){
+        defaultImg.classList.add("hidden");
+    }else{
+        defaultImg.classList.remove("hidden");
+    }
     
+    diplayNameElement.textContent = diplayName;
 }
 
 
